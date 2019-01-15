09c7deef5028de4f3a601d50eaf44eea5b768161
Khem Myrick <khem.myrick@gmail.com>
2019-01-15 09:48:05 -0800

    Added default renderer setting and updated views.
    Added default renderer classes to DRF settings.
    
    Added imports to pugorugh views.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,7 +116,11 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
 }
 
 
--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -1,14 +1,26 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
+from rest_framework import status
 from rest_framework import mixins
 from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import (CreateAPIView, RetrieveUpdateAPIView,
+                                     GenericAPIView)
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
 from . import models
 from . import serializers
 
+
+AGES = {
+    'b': [i for i in range(10)],
+    'y': [i for i in range(10, 35)],
+    'a': [i for i in range(35, 80)],
+    's': [i for i in range(80, 250)]
+}
 
 class UserRegisterView(CreateAPIView):
     permission_classes = (permissions.AllowAny,)
@@ -21,26 +33,39 @@ class UserRegisterView(CreateAPIView):
     userpref = models.UserPref(
         user=model,
         age='b',
-        gender='f',
+zx        gender='f',
         size='s'
     )
     userpref.create()
     '''
     
-
-class UserPrefDetailView(RetrieveUpdateAPIView):
+class UserPrefDetailView(mixins.CreateModelMixin,
+                         mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin,
+                         GenericAPIView):
     '''
+    viewclass formally known as class UserPrefDetailView(RetrieveUpdateAPIView):    
     Review or update user preferences.
     '''
+    ## Should be Create Retrieve Update APIView.
+    ## Selecting nothing is the same as selecting everything.
     model = models.UserPref
     lookup_field = 'pk'
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     serializer_class = serializers.UserPrefSerializer
-    user = self.request.user
-    queryset = models.UserPref.objects.filter(user=self.user)
+    ## user = self.request.user
+    ## queryset = models.UserPref.objects.filter(user=self.user)
+    queryset = models.UserPref.objects.all()
+    
+    def post(self, request, format=None):
+        serializer = serializers.UserPrefSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response(serializer.data, status=status.HTTP_201_CREATED)
 
     def get_queryset(self):
-        return models.UserPref.objects.filter(user=self.request.user)
+        return models.UserPref.objects.all()
+        # return models.UserPref.objects.filter(user=self.request.user)
 
     def get_object(self):
         print('UserPref queryset: ', self.queryset)
